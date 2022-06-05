63756014d7ae4104bc44974cab871bd649ecc9c1
ak9675 <98090519+ak9675@users.noreply.github.com>
2022-06-05 16:26:45 +0000

    Update newrg.tf

--- newrg.tf
+++ newrg.tf
@@ -6,3 +6,9 @@ resource "azurerm_resource_group" "example" {
   name     = "tfcloudrg"
   location = "West Europe"
 }
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "tfcloudrg1"
+  location = "eastus"
+}
